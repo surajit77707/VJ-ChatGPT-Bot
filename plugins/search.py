0bccb2b1b0d08d5b21728c9e4045fa29974fa79a
surajit77707 <surajitsingh77707@gmail.com>
2025-02-01 00:13:17 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -29,7 +29,7 @@ async def ai_answer(client, message):
                     max_tokens=1200,  # Increase the value of max_tokens to allow for longer responses
                     temperature=0.6
                 )
-                footer_credit = "<b><a href='https://t.me/vj_bot_disscussion'>• ʀᴇᴘᴏʀᴛ ɪꜱꜱᴜᴇ •</a>══<a href='https://t.me/kingvj01'>• ᴄᴏɴᴛᴀᴄᴛ ᴍᴀꜱᴛᴇʀ •</a></b>"
+                footer_credit = "<b><a href='https://t.me/Venom_is_alive'>• 𝖢𝖮𝖭𝖳𝖠𝖢𝖳 𝖬𝖠𝖲𝖳𝖤𝖱 •</a></b>"
                 ai_response = response.choices[0].message.content.strip()
                 await msg.delete()
                 await send_message_in_chunks(client, message.chat.id, f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴀɴsᴡᴇʀ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ** 👇\n\n{ai_response}\n\n{footer_credit}")
@@ -37,6 +37,6 @@ async def ai_answer(client, message):
                 
             except Exception as error:
                 print(error)
-                await message.reply_text(f"**An error occurred:**\n\n**{error}**\n\n**Forward This Message To @KingVJ01**")
+                await message.reply_text(f"**An error occurred:**\n\n**{error}**\n\n**𝓕𝓸𝓻𝔀𝓪𝓻𝓭 𝓣𝓱𝓲𝓼 𝓜𝓮𝓼𝓼𝓪𝓰𝓮 𝓣𝓸 @𝓥𝓮𝓷𝓸𝓶_𝓲𝓼_𝓪𝓵𝓲𝓿𝓮**")
     else:
         return
